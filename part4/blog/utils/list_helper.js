b559b6d458d90900872ad686ed70d9d59ab1cf31
Winston Thai <winstonthai1@gmail.com>
2024-05-06 00:09:22 -0600

    exercise 4.5 - favoriteBlog helper function and unit tests

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,24 @@ const totalLikes = (blogs) => {
     return blogs.reduce((accumulator, currentBlog) => accumulator + currentBlog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) { return {} }
+
+    let highestLikesBlog = blogs[0]
+
+    blogs.forEach(blog => {
+        if (blog.likes > highestLikesBlog.likes) { highestLikesBlog = blog }
+    })
+
+    return {
+        title: highestLikesBlog.title,
+        author: highestLikesBlog.author,
+        likes: highestLikesBlog.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
