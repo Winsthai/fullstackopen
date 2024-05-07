f0130bfc88cfad578a7125cfb305c7bc8ca38cf2
Winston Thai <winstonthai1@gmail.com>
2024-05-06 22:01:16 -0600

    exercise 4.8 - verifies that the blog list application returns the correct amount of blog posts in the JSON format on GET request

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,13 +1,29 @@
-const { test, describe, after } = require('node:test')
+const { test, describe, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const helper = require('./test_helper')
 const app = require('../app')
 
 const api = supertest(app)
 
-describe('get request', () => {
+const Blog = require('../models/blog')
 
+// Before each test, clear the database, then add all blogs from initialBlogs back to it
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    const blogs = helper.initialBlogs.map(blog => new Blog(blog))
+    const promiseArray = blogs.map(blog => blog.save())
+    await Promise.all(promiseArray)
+})
+
+test('returns correct amount of blogs in JSON format', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
 })
 
 after(async () => {
