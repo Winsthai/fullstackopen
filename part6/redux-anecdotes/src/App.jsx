b9ffc91b4012383cbc8f8573263dc1487c29dca1
Winston Thai <winstonthai1@gmail.com>
2024-07-06 00:28:45 -0600

    exercise 6.8 - separate rendering of anecdote list into new component

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,29 +1,11 @@
 import AnecdoteForm from './components/AnecdoteForm'
-import { incrementVote } from './reducers/anecdoteReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes ))
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(incrementVote(id))
-  }
 
   return (
     <div>
-      <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList></AnecdoteList>
       <AnecdoteForm></AnecdoteForm>
     </div>
   )
