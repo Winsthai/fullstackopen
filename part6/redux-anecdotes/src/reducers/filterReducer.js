b6642cd53e97c837629170f32714dfd7e6e36e2a
Winston Thai <winstonthai1@gmail.com>
2024-07-09 03:11:13 -0600

    exercise 6.10 - refactor filter reducer

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,20 +1,16 @@
-const filterReducer = (state = 'NO_FILTER', action) => {
-    //console.log('state now: ', state)
-    //console.log('action', action)
+import { createSlice } from "@reduxjs/toolkit"
 
-    switch (action.type) {
-        case 'SET_FILTER':
+const initialState = 'NO_FILTER'
+
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState,
+    reducers: {
+        filterChange(state, action) {
             return action.payload
-        default:
-            return state
+        }
     }
-}
+})
 
-export const filterChange = filter => {
-    return {
-        type: 'SET_FILTER',
-        payload: filter,
-    }
-}
-
-export default filterReducer
+export const {filterChange} = filterSlice.actions
+export default filterSlice.reducer
