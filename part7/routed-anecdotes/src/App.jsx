f84aaacab77d2464881512175979bfdb0c7460a8
Winston Thai <winstonthai1@gmail.com>
2024-07-17 02:54:37 -0600

    exercise 7.1 - integrate react router into application

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react'
+import {
+  Routes, Route, Link
+} from 'react-router-dom'
 
 const Menu = () => {
   const padding = {
@@ -6,9 +9,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link to='/' style={padding}>anecdotes</Link>
+      <Link to='/create' style={padding}>create new</Link>
+      <Link to='/about' style={padding}>about</Link>
     </div>
   )
 }
@@ -126,9 +129,11 @@ const App = () => {
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Routes>
+        <Route path='/' element={ <AnecdoteList anecdotes={anecdotes}></AnecdoteList> } ></Route>
+        <Route path='/create' element={ <CreateNew></CreateNew> } ></Route>
+        <Route path='/about' element={ <About></About> } ></Route>
+      </Routes>
       <Footer />
     </div>
   )
