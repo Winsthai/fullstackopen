15c489a0a65ed4acf6abdc532a43a94b579af424
Winston Thai <winstonthai1@gmail.com>
2024-07-19 03:59:59 -0600

    exerecise 7.8 - implement backend communication as a custom hook

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -18,10 +18,19 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
 
-  const create = (resource) => {
-    // ...
+    getData()
+  }, [baseUrl])
+
+  const create = async (resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+    return response.data
   }
 
   const service = {
