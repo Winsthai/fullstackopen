1b1fa3701cb282155caed9a4b5cca1247ddce505
Winston Thai <winstonthai1@gmail.com>
2024-07-26 03:15:37 -0600

    exercise 8.3

--- part8/library/index.js
+++ part8/library/index.js
@@ -102,10 +102,16 @@ const typeDefs = `
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -114,6 +120,13 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: () => books,
+    allAuthors: () =>
+      authors.map((author) => {
+        return {
+          name: author.name,
+          bookCount: books.filter((book) => book.author === author.name).length,
+        };
+      }),
   },
 };
 
